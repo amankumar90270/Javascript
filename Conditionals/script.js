a4b99f890a21ac0a9c99b64b11e8c1927414d380
Aman Kumar <amankumar731841@gmail.com>
2025-10-05 13:24:21 +0530

    learn conditions

---
+++ Conditionals/script.js
@@ -0,0 +1,46 @@
+// let x=10, y=20
+// console.log(x==y)
+
+// Convert variable type
+// let z="10"
+// let a = (Number(z))
+// console.log(typeof a)
+
+// Difference Between "==" and "==="
+// let x = 20;
+// let y = "20";
+// console.log(x == y); //convert type before check
+// console.log(x===y); // strictly check type
+
+// Loop
+// for loop
+// for(let i=0; i<=10; i++){
+//     console.log("Aman")
+// }
+
+// while Loop
+// let i=0;
+// while(i<=10){
+//     console.log("Aman")
+//     i++;
+// }
+
+// DO while Loop
+// let i=0;
+// do{
+//     console.log("Aman");
+//     i++
+// }while(i<=10);
+
+
+// if else condition
+let age=18;
+if(age<10){
+    console.log("You are child")
+}else if(age<=18){
+    console.log("You are young")
+}else if(age<50){
+    console.log("You are a teen")
+} else{
+    console.log("You are old ")
+}
